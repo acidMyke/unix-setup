be226daf28e7bd09ebc0b14c49e95be59f34a622
Acid Myke <13977709+acidMyke@users.noreply.github.com>
2022-04-17 21:25:33 +0800

    added prompt hint

--- shell/server/fedora.zsh.sh
+++ shell/server/fedora.zsh.sh
@@ -4,15 +4,19 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 
 #xclip
+echo "Installing Xclip"
 dnf install xclip
 
 # zsh
+echo "Installing ZSH"
 dnf install zsh
 
 # Starship Prompt
+echo "Installing Starship Prompt"
 curl -sS https://starship.rs/install.sh | sh
 
-# NVM
+# nvm
+echo "Installing nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
 nvm install --lts
 
